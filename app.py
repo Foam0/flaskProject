672783f3f24d971e463edd85d59a4938ef7badbd
MaxChernous <94230811+MaxChernous@users.noreply.github.com>
2022-05-03 23:32:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -22,78 +22,6 @@ boards = database.board
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return flask.render_template('gt.html')
-
-
-@app.route('/reg', methods=['GET', 'POST'])
-def reg_parse():
-    name = (request.values['login'])
-    key1 = (request.values['psw1'])
-    key2 = (request.values['psw2'])
-    if key1 != key2:
-        resp = (flask.render_template("gt.html", message='Passwords doesn\'t match'))
-    elif not users.count_documents({"name": name}):
-        users.insert_one({"id": _64(), "name": name, "key": key1})
-        resp = flask.redirect('/list')
-        cookie = users.find_one({"name": name})["id"]
-        resp.set_cookie('userID', cookie)
-    else:
-        resp = flask.render_template("gt.html", message='Such login already exists')
-    return resp
-
-
-@app.route('/auth', methods=['GET', 'POST'])
-def authorize():
-    name = (request.values['login'])
-    key = (request.values['psw'])
-    if users.count_documents({"name": name}):
-        if users.find_one({"name": name})["key"] == key:
-            resp = make_response(flask.redirect("/list"))
-            cookie = users.find_one({"name": name})["id"]
-            resp.set_cookie('userID', cookie)
-            return resp
-    return flask.render_template("gt.html", message='Incorrect login/password')
-
-
-@app.route('/list', methods=['post', 'get'])
-def main():
-    userID = request.cookies.get("userID")
-    name = users.find_one({"id": userID})["name"]
-    if userID == '': return flask.redirect('/')
-    try:
-        lst = list(boards.find({"users": userID}))
-        for ind, e in enumerate(lst):
-            m = []
-            for i in e["users"]:
-                m.append(users.find_one({"id": i})["name"])
-            lst[ind]["users"] = m
-    except IndexError:
-        lst = []
-    d = []
-    for j in lst:
-        cnt_todo = 0
-        cnt_in_progress = 0
-        cnt_done = 0
-        count_mine = 0
-        for task in j["notes"]:
-            count_mine += task["contributors"] == (str(name))
-            if task["status"] == "to do":
-                cnt_todo += 1
-            if task["status"] == "in progress":
-                cnt_in_progress += 1
-            if task["status"] == "done":
-                cnt_done += 1
-        count_mine = str(count_mine)
-        cnt_todo = str(cnt_todo)
-        cnt_done = str(cnt_done)
-        cnt_in_progress = str(cnt_in_progress)
-        d.append([j['name'], count_mine, cnt_todo, cnt_in_progress, cnt_in_progress, j["id"]])
-    return flask.render_template("Projects table.html", lst=d, nickname=users.find_one({"id": userID})["name"])
-
-
 @app.route('/list/add', methods=['POST'])
 def add():
     userID = request.cookies.get("userID")
@@ -123,24 +51,6 @@ def add():
                 "time": time(),
                 "desc": ''
             }            '''
-
-
-@app.route('/list/new_user', methods=['post', 'get'])
-def new_user():  # new user in list
-    userID = request.cookies.get("userID")
-    boardID = request.args.get("id")
-    boardID = boardID.split()[0]
-    if not boards.count_documents({"users": userID}): boards.update_one({"id": boardID}, {"$push": {"users": userID}})
-    return flask.redirect('/list')
-
-
-@app.route("/list/board/task/update", methods=['get', 'post'])
-def update_status():
-    boardID = request.args.get("boardID")
-    status_task = request.args.get("status")
-    task_id = request.args.get("taskID")
-    boards.update_one({"id": boardID, "notes.id": task_id}, {"$set": {"notes.$.status": status_task}})
-    return flask.redirect(f"/list/board?id={boardID}")
 
 
 @app.route('/list/board/task/add', methods=['POST', 'GET'])
@@ -182,24 +92,10 @@ def mai1():
     resp = flask.render_template("board.html", lst=lst, un=username, boardID=boardID, status=status)
     return resp
 
-
-@app.route('/list/del_board', methods=['post', 'get'])
-def de_l():
-    boardID = request.values['id']
-    userID = request.cookies.get("userID")
-    boards.update_one({"id": boardID}, {'$pull': {"users": userID}})
-    return flask.redirect('/list')
-
-
 @app.route('/list/board/exit', methods=['post'])
 def exxit():
     return flask.redirect('/list')
 
-
-@app.route('/list/board', methods=['POST'])
-def go_to_board():
-    boardID = request.query_string.get('id')
-    resp = make_response(flask.redirect(f"/board?id={boardID}"))
     return resp
 
 
